bae1345c6eb511e34881b65cb87892a2facf0f60
Milan <bio.skop@yahoo.com>
2016-10-05 14:51:27 +0200

    part3

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -1,21 +1,25 @@
 <?php
 
 /*
-	
-@package sunsettheme
-	
 	========================
 		ADMIN PAGE
 	========================
 */
 
 function sunset_add_admin_page() {
-	
-	add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd-sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
-	
+
+	//Generate sunset admin page
+	add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page', 'dashicons-smiley', 110 );
+
+	//Generate sunset admin sub-pages
+	add_submenu_page( 'alecaddd_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page');
+	add_submenu_page( 'alecaddd_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'alecaddd_sunset_css', 'sunset_theme_settings_page');
 }
 add_action( 'admin_menu', 'sunset_add_admin_page' );
 
 function sunset_theme_create_page() {
+	require_once( get_template_directory() . '/inc/templates/sunset-admin.php' );
+}
+function sunset_theme_settings_page() {
 	//generation of our admin page
 }
